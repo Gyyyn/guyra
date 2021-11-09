4afa1453179b7d173e5e77d9fa0ab4fb8a520692
Gyyyn <gr3engoon@gmail.com>
2021-11-09 17:26:50 -0300

    Add file upload indicator and activate notification button

--- assets/js/study.js
+++ assets/js/study.js
@@ -27,6 +27,7 @@ if (getCookie('dismissed') == "true") {
 responseArea = localStorage.getItem('responseArea');
 commentArea = document.querySelector("#comment");
 submitButton = document.querySelector("#submit");
+theInput = document.getElementById('file_upload_input');
 
 if (responseArea !== null) {
   commentArea.value = responseArea;
@@ -34,6 +35,7 @@ if (responseArea !== null) {
 
 commentArea.onkeyup = eventTrigger;
 submitButton.onclick = submitTrigger;
+theInput.addEventListener('input', fileUploadTrigger);
 
 function eventTrigger(e) {
   localStorage.setItem('responseArea', commentArea.value);
@@ -41,4 +43,10 @@ function eventTrigger(e) {
 
 function submitTrigger(e) {
   localStorage.setItem('responseArea', '');
+}
+
+function fileUploadTrigger(e) {
+  if (theInput.files.length != 0) {
+    document.getElementById('file_list').innerHTML = theInput.files[0].name;
+  }
 }
