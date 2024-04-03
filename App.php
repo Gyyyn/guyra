f13319294e450e7990a0bb63d0df3b6ac3f2f1bb
Gyyyn <gr3engoon@gmail.com>
2024-04-03 19:03:20 -0300

    update ver to 0.8

--- App.php
+++ App.php
@@ -2,7 +2,7 @@
 
 // Define the app version.
 if (!defined('GUYRA_VERSION'))
-define('GUYRA_VERSION', '0.7.3');
+define('GUYRA_VERSION', '0.8.0');
 
 // Initialize the App enviroment
 include_once './functions/Init.php';
